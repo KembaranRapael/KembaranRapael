bd387b986bbd687ff7198ce2bba54e5b1a63f4ea
KembaranRapael <81946127+KembaranRapael@users.noreply.github.com>
2021-04-05 11:39:36 +0700

    Add files via upload

---
+++ Aris.M/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Daftar Siswa SMK Pesat </h1>
+    <table border="1" cellpadding="10" cellxpacing="0">
+
+    <tr> 
+    <th>no</th>
+    <th>gambar</th>
+    <th>NIS</th>
+    <th>Nama</th>
+    <th>email</th>
+    <th>jurusan</th>
+    <th>aksi</th>
+    </tr>
+
+    <?php> $i; ?>
+    <?php foreach (siswa_smk as $row): ?>
+</body>
+</html>
